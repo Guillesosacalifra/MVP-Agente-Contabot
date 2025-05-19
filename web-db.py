0fc93d927486f90eaead1b78e9eafb1f002667ee
Guillesosacalifra <scguille98@gmail.com>
2025-05-19 09:31:39 -0300

    Update web-db.py

--- web-db.py
+++ web-db.py
@@ -8,13 +8,6 @@ almacenadas en una base de datos SQLite.
 # =======================
 # 📦 IMPORTACIONES
 # =======================
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import datetime
 from tqdm import tqdm
